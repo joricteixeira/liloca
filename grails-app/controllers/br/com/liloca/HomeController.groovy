b38a19d953f84425fdef87acc5d1a7aa7544dab8
Felipe <felipesantos.uerj@gmail.com>
2015-06-13 15:18:56 -0300

    Permitir apenas 3 temas destaques!

--- grails-app/controllers/br/com/liloca/HomeController.groovy
+++ grails-app/controllers/br/com/liloca/HomeController.groovy
@@ -4,11 +4,16 @@ class HomeController {
 
     def index() {
 
-
-
-
         //TODO adicionar lógica para obter somente destaques
-        def temas = Tema.findAllByDestaque(true)
+        def temas = Tema.findAllByDestaque(true);
+
+        //Se houver mais de 3 temas selecionados como destaque, retirar o excesso e mostrar apenas 3
+        def qtdTemas = temas.size()
+        if (qtdTemas > 3) {
+            for(qtdTemas; qtdTemas > 3; qtdTemas--) {
+                temas.remove(1)
+            }
+        }
 
         render(view: '/home/home', model: [temasDestaque: temas])
     }
