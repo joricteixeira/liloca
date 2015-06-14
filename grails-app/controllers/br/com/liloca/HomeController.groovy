79b183854b4b820470ffb61c15db213f9c5a6c5f
Felipe <felipesantos.uerj@gmail.com>
2015-06-13 23:48:55 -0300

    US Concluida.

--- grails-app/controllers/br/com/liloca/HomeController.groovy
+++ grails-app/controllers/br/com/liloca/HomeController.groovy
@@ -4,16 +4,7 @@ class HomeController {
 
     def index() {
 
-        //TODO adicionar lógica para obter somente destaques
-        def temas = Tema.findAllByDestaque(true);
-
-        //Se houver mais de 3 temas selecionados como destaque, retirar o excesso e mostrar apenas 3
-        def qtdTemas = temas.size()
-        if (qtdTemas > 3) {
-            for(qtdTemas; qtdTemas > 3; qtdTemas--) {
-                temas.remove(1)
-            }
-        }
+        def temas = Tema.findAllByDestaque(true, [max: 3])
 
         render(view: '/home/home', model: [temasDestaque: temas])
     }
