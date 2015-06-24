e019f300aaf870dcf982ae10faa6a07d38b23369
joricteixeira@gmail.com <adh#4815162342>
2015-06-24 11:22:51 -0300

    Outros acertos

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,7 +16,8 @@ grails.project.fork = [
     // configure settings for the test-app JVM, uses the daemon by default
     test: false,//test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
     // configure settings for the run-app JVM
-    run: false, //run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    //run: false,
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
     // configure settings for the run-war JVM
     war: false, //war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
     // configure settings for the Console UI JVM
